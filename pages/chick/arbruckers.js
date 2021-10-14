cf8de52e927ccb214ead75304a26f0e5b4a0e50d
vicfw <faridforward2223@gmail.com>
2021-10-14 15:10:59 +0330

    fixing issue

--- pages/chick/arbruckers.js
+++ pages/chick/arbruckers.js
@@ -74,11 +74,15 @@ const Arbruckers = ({ data }) => {
 
 export async function getStaticProps(context) {
   const url = encodeURI("products?category=آبراکرز پلاس");
-  const res = await axiosInstance.get(url);
-  return {
-    props: { data: res.data.data },
-    revalidate: 10,
-  };
+  try {
+    const res = await axiosInstance.get(url);
+    return {
+      props: { data: res.data.data },
+      revalidate: 10,
+    };
+  } catch (error) {
+    return { props: { data: [] } };
+  }
 }
 
 export default Arbruckers;
