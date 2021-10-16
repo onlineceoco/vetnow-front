061d790473fc52138c6727a91bbe952657b5508d
vicfw <faridforward2223@gmail.com>
2021-10-16 13:45:25 +0330

    pwa

--- next.config.js
+++ next.config.js
@@ -25,10 +25,9 @@ module.exports = phase => {
 };
 module.exports = withPWA({
   pwa: {
-    // dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    register: true,
+    dest: "public",
+    // register: true,
     // scope: '/app',
-    sw: "/sw.js",
+    // sw: "/sw.js",
   },
 });
